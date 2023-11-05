878fdc3e79ae5993de353bb75dededba4819b65b
mwangisam203 <mwangisam203@gmail.com>
2023-11-05 16:26:54 +0300

    completed lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { actors } from "../data";
+import React from 'react';
+import { actors } from '../data'; // Import actors from data.js
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, i) => (
+              <li key={i}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
